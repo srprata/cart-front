3ea5229906225653a9984b85494ed620179aaee9
srprata <51327011+srprata@users.noreply.github.com>
2021-04-04 19:45:06 -0300

    Heroku
    Alterações para rodar heroku

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   //dev
-  const url = 'http://localhost:8080/graphql';
+  // const url = 'http://localhost:8080/graphql';
   //heroku
-  //const url = `https://app-cart-service.herokuapp.com/graphql`
+  const url = `https://app-cart-service.herokuapp.com/graphql`
 
   const client = new ApolloClient({
     uri: url,
